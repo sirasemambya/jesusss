9146f2b1bfce52d4eca853c4fe203b90c2ac5420
Sira Semambya <sirasemambya@gmail.com>
2018-02-07 18:11:02 -0500

    dfs
    dsd

---
+++ product.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "array_product <br>";
+
+
+$a = array(2, 4, 6, 8);
+echo "product(a) = " . array_product($a) . "\n";
+echo "product(array()) = " . array_product(array()) . "\n";
+
+?>
